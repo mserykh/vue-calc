8bd961daad47fa2f1c40240f5acd413a72cb58ae
Marie Serykh <marie.serykh@gmail.com>
2020-04-20 23:16:49 +0200

    Added a simple markdown of a calculator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,7 +1,25 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
-    
+  <div class="calculator">
+    <div class="display">0.25456</div>
+    <div class="btn">C</div>
+    <div class="btn">+/-</div>
+    <div class="btn">%</div>
+    <div class="btn">÷</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn">x</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn">-</div>
+    <div class="btn">1</div>
+    <div class="btn">2</div>
+    <div class="btn">3</div>
+    <div class="btn">+</div>
+    <div class="zero btn">0</div>
+    <div class="btn">.</div>
+    <div class="btn">=</div>
   </div>
 </template>
 
@@ -13,4 +31,19 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .calculator {
+    font-size: 40px;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: minmax(50px, auto);
+  }
+
+  .display {
+    grid-column: 1 / 5;
+    background-color: red;
+  }
+
+  .zero {
+    grid-column: 1 / 3;
+  }
 </style>
